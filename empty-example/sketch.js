60685f7573ae9c6658977e31b06b2294f0f66e26
melindaliu <meilnda.r.liu@gmail.com>
2019-10-17 13:46:50 -0700

    Update sketch.js

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,9 +1,29 @@
 
 function setup() {
   createCanvas(800, 800);
+  class Ball {
+
+ constructor(x,y,color){
+		this.x = x;
+    		this.y = y;
+           this.color = color;
+	  }
+}
 }
 
 function draw() {
 	background('white');
+  drawBall(){
+    	   stroke(0);
+    	   fill(this.color);
+         ellipse(this.x,this.y,10,10);
+	}
+
+  moveBall(){ 
+	   this.x = this.x+2;
+	   this.y = this.y+.5;
+	}
+
+
 
 }
