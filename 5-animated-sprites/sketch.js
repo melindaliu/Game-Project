176b3ef7855af03672eefcee947a7fcf1de8cdf9
melindaliu <meilnda.r.liu@gmail.com>
2019-11-06 08:48:58 -0800

    Update sketch.js

--- 5-animated-sprites/sketch.js
+++ 5-animated-sprites/sketch.js
@@ -10,7 +10,7 @@ function preload() {
 //
 //   //create an animation from a sequence of numbered images
 //   //pass the first and the last file name and it will try to find the ones in between
-  ghost = loadAnimation('assets/ghost_standing0001.png', 'assets/ghost_standing0007.png');
+  ghost = loadAnimation('sprites/sprite001.png', 'assets/sprite001.png');
 
 }
 
